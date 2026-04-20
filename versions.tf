ab0349fe42b5079d3afc62fc4a58934acb7be99f
Josh Luedeman <josh@joshluedeman.com>
2026-04-19 21:28:41 -0400

    fix: rename provider local name to popsrox and remove stale lock file

--- versions.tf
+++ versions.tf
@@ -7,9 +7,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.116"
     }
-    popsrox-utils = {
+    popsrox = {
       source  = "POps-Rox/azutils"
-      version = "~> 1.0.4"
+      version = "~> 1.0"
     }
     random = {
       source  = "hashicorp/random"
